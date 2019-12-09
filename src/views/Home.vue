e68b7f456ad239619300b0a04f361d8ae3e53114
donnajobridge <donnajb@gmail.com>
2019-12-08 21:04:03 -0600

    random articles on front page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <v-container fluid>
     <v-row justify='center'>
-      <v-col cols=6 sm=4 md=3 v-for='(card, index) in frontPage' :key='index'>
+      <v-col cols=6 sm=4 md=3 v-for='(card, index) in randomArticles' :key='index'>
         <v-card flat color='grey lighten-3' height='auto' :href="getArticleLink(card)" target='_blank'>
           <v-img max-height='400px' contain :src="getImage(card.image)">
             </v-img>
@@ -26,7 +26,15 @@ export default {
       return this.allArticles
       .filter(obj => obj.frontPageSpot>0)
       .sort((a,b)=>(a.frontPageSpot > b.frontPageSpot)? 1 : -1)
-    }
+    },
+    randomArticles(){
+      let n=12;
+      // Shuffle array
+      const shuffled = this.allArticles.sort(() => 0.5 - Math.random());
+      // Get sub-array of first n elements after shuffled
+      let selected = shuffled.slice(0, n);
+      return selected;
+      },
   },
   methods: {
     getImage: function(name){
